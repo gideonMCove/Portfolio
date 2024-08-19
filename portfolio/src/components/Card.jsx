4b940487749614a32eb43f037d61c38ed379dffe
GideonCove <gideoncove@gmail.com>
2024-08-19 14:37:09 -0400

    initial commit

---
+++ portfolio/src/components/Card.jsx
@@ -0,0 +1,20 @@
+
+export default function Card () {
+    return (
+        <div className="Card">
+         <h1 className="Name"> Gideon Cove </h1>
+         <ul>
+            <l1 className="Email">GideonCove@gmail.com</l1>
+            <l1 className="cell">484-747-0177</l1>
+         </ul>
+         <a href="https://github.com/gideonMCove" className="Github" target="_blank">Github</a>
+         <a href="https://www.linkedin.com/in/gideon-cove-86532527a/" className="Linkedin" target="_blank">Linkedin</a>
+         <ul className="highlights">
+            <h1>Highlights:</h1>
+            <a href="https://github.com/kfharoun/dnd_tracker" target="blank">Adeventures Atlas</a>
+            <a href="https://github.com/gideonMCove/Trivialis-Serpentes" target="_blank">Trvialis-Serpentes</a>
+            <a href="https://github.com/gideonMCove/u3_react_axios_lab" target="_blank">Star Wars API</a>
+         </ul>
+        </div>
+    )
+}
